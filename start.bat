f16a7d77c5848abd4fea43a9aa8a4488db2bec8c
Rcpentuy <rcpentuy@gmail.com>
2024-09-10 20:32:50 +0800

    fixes

--- start.bat
+++ start.bat
@@ -2,11 +2,14 @@
 REM 激活虚拟环境（假设你使用的是 venv）
 call .venv\Scripts\activate
 
+set HTTP_PROXY=http://127.0.0.1:7890
+set HTTPS_PROXY=http://127.0.0.1:7890
+
 REM 更新代码
 git pull origin main
 
 REM 安装或更新依赖
-pip install -r requirements.txt
+pip --proxy http://127.0.0.1:7890 install -r requirements.txt
 
 REM 启动服务器
 gunicorn wsgi:app
